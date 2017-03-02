b0346b9dc29dda53f1913a4f737ca14fede7ffb2
Benjamin Weigang <Benjamin.Weigang@mailbox.org>
2017-03-02 17:08:33 +0100

    Added NET library to calculate nesting numbers

--- scripts/utility.py
+++ scripts/utility.py
@@ -2,6 +2,39 @@ import networkx as nx
 import csv
 import os
 
+from scripts.decomposer import sorted_connected_components, \
+                       remove_intersecting_edges, \
+                       prune_graph, \
+                       apply_workaround
+
+from scripts.analyzer import analyze_tree
+
+
+def clean_graph(graph):
+    print("Removing disconnected parts")
+    con = sorted_connected_components(graph)
+    assert len(con) != 0, 'Graph is empty!'
+
+    graph = con[0]
+
+    print("Removing intersecting edges.")
+    remove_intersecting_edges(graph)
+
+    print("Pruning.")
+    pruned = prune_graph(graph)
+
+    print("Applying workaround to remove spurious collinear edges.")
+    removed_edges = apply_workaround(pruned)
+
+    print("Pruning again.")
+    pruned = prune_graph(pruned)
+
+    con = sorted_connected_components(pruned)
+    print("Connected components:", len(con))
+    assert len(con) != 0, 'Graph is empty!'
+
+    return con[0]
+
 
 def graph_from_data(node_path, edge_path):
     """
@@ -25,15 +58,27 @@ def graph_from_data(node_path, edge_path):
     with open(edge_path) as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quoting=csv.QUOTE_NONNUMERIC)
         for row in reader:
-            edges.append({'node_id1': int(row[0]), 'node_id2': int(row[1]), 'radius': row[2], 'length': row[3]})
+            edges.append({
+                'node_id1': int(row[0]),
+                'node_id2': int(row[1]),
+                'radius': row[2],
+                'length': row[3]
+            })
 
     # Create the graph with the networkx library
     G = nx.Graph()
     for node in nodes:
         G.add_node(node['node_id'], pos=(node['x'], node['y']))
     for edge in edges:
-        G.add_edge(edge['node_id1'], edge['node_id2'], weight=edge['radius'], length=edge['length'])
+        G.add_edge(
+            edge['node_id1'],
+            edge['node_id2'],
+            weight=edge['radius'],
+            length=edge['length'],
+            conductivity=1
+        )
 
+    #  return network_id, clean_graph(G)
     return network_id, G
 
 
@@ -59,3 +104,14 @@ def graph_generator():
             # we raise i to i+1, get the paths for the corresponding files and generate the graph
             # for these files.
             yield graph_from_data(node_path, edge_path)
+
+
+def get_nesting_numbers(tree):
+    horton_strahler, shreve, marked_tree, tree_no_ext, \
+    marked_tree_no_ext, tree_asymmetry, tree_asymmetry_no_ext, \
+    areas = analyze_tree(tree)
+
+    nesting_number = 1 - tree_asymmetry
+    nesting_number_no_ext = 1 - tree_asymmetry_no_ext
+
+    return nesting_number, nesting_number_no_ext
